2962dac12a5f6f088094d3d98c243c69700af6ff
Michal Wieja <m.wieja@qarbon.it>
2022-10-01 13:18:59 +0200

    schedule config

--- config/schedule.js
+++ config/schedule.js
@@ -1,7 +1,7 @@
 export const scheduleConfig = [{
-  label: 'dla mlodszych',
+  label: 'ZAJĘCIA DLA MŁODSZYCH',
   children: [{
-    label: 'malarstwo',
+    label: 'Malarstwo (wiek: 7-11 lat)',
     event: [{
       day: 'wtorek',
       hour: '16:30-17:30',
@@ -9,27 +9,183 @@ export const scheduleConfig = [{
     }]
   },
   {
-    label: 'szkola rysunku',
+    label: 'Szkoła rysunku (wiek: 7-11 lat)',
     event: [{
       day: 'wtorek',
       hour: '17:45-18:45',
       time: '1.0 godz'
     }, {
-      day: 'sroda',
+      day: 'środa',
       hour: '09:00-10:00',
+      time: '1.0 godz'
+    }]
+  },
+  {
+    label: 'Ceramika (wiek: 7-11 lat)',
+    event: [{
+      day: 'środa',
+      hour: '09:15-10:15',
+      time: '1.0 godz'
+    }, {
+      day: 'czwartek',
+      hour: '16:30-17:30',
       time: '1.0 godz'
     }]
   }]
 },
 {
-  label: 'dla starszych',
+  label: 'ZAJĘCIA DLA STARSZYCH',
   children: [{
-    label: 'malarstwo',
+    label: 'Malarstwo (wiek: 12+)',
     event: [{
       day: 'wtorek',
       hour: '17:45-18:45',
       time: '1.5 godz'
     }]
-  }
-  ]
+  },
+  {
+    label: 'Szkoła rysunku (wiek: 12+)',
+    event: [{
+      day: 'wtorek',
+      hour: '16:00-17:30',
+      time: '1.5 godz'
+    },
+    {
+      day: 'środa',
+      hour: '10:15-11:45',
+      time: '1.5 godz'
+    }]
+  },
+  {
+    label: 'Ceramika (wiek: 12+)',
+    event: [{
+      day: 'środa',
+      hour: '10:30-12:00',
+      time: '1.5 godz'
+    },
+    {
+      day: 'czwartek',
+      hour: '18:00-19:30',
+      time: '1.5 godz'
+    }]
+  },
+  {
+    label: 'Akwarele (wiek: 10+)',
+    event: [{
+      day: 'piątek',
+      hour: '16:30-18:00',
+      time: '1.5 godz'
+    }
+    ]
+  }]
+},
+{
+  label: 'ZAJĘCIA DLA WSZYSTKICH',
+  children: [{
+    label: 'Origami',
+    event: [{
+      day: 'czwartek',
+      hour: '16:15-17:15',
+      time: '1.0 godz'
+    }, {
+      day: 'piątek',
+      hour: '09:00-10:00',
+      time: '1.0 godz'
+    }]
+  },
+  {
+    label: 'Rękodzieło',
+    event: [{
+      day: 'czwartek',
+      hour: '17:30-19:00',
+      time: '1.5 godz'
+    },
+    {
+      day: 'piątek',
+      hour: '10:15-11:45',
+      time: '1.5 godz'
+    }]
+  },
+  {
+    label: 'Grafika',
+    event: [{
+      day: 'piątek',
+      hour: '10:30-12:00',
+      time: '1.5 godz'
+    },
+    {
+      day: 'piątek',
+      hour: '14:45-16:15',
+      time: '1.5 godz'
+    }]
+  }]
+},
+{
+  label: 'MUZYKA',
+  children: [{
+    label: 'Ukulele (gr. początkująca)',
+    event: [{
+      day: 'sobota',
+      hour: '10:30-11:30',
+      time: '1.0 godz'
+    }]
+  },
+  {
+    label: 'Ukulele (gr. zaawansowana)',
+    event: [{
+      day: 'sobota',
+      hour: '11:45-12:45',
+      time: '1.0 godz'
+    }]
+  },
+  {
+    label: 'Gitara (zajęcia indywidualne lub duet)',
+    event: [{
+      day: 'środa',
+      hour: '14:00-15:00',
+      time: '1.0 godz'
+    }, {
+      day: 'środa',
+      hour: '15:00-16:00',
+      time: '1.0 godz'
+    }, {
+      day: 'środa',
+      hour: '16:00-17:00',
+      time: '1.0 godz'
+    }, {
+      day: 'środa',
+      hour: '17:00-18:00',
+      time: '1.0 godz'
+    }]
+  },
+  {
+    label: 'Muzykoterapia (zajęcia indywidualne)',
+    event: [{
+      day: 'środa',
+      hour: '14:00-15:00',
+      time: '1.0 godz'
+    }, {
+      day: 'środa',
+      hour: '15:00-16:00',
+      time: '1.0 godz'
+    }, {
+      day: 'środa',
+      hour: '16:00-17:00',
+      time: '1.0 godz'
+    }, {
+      day: 'środa',
+      hour: '17:00-18:00',
+      time: '1.0 godz'
+    }]
+  }]
+}, {
+  label: 'ZAJĘCIA DLA SENIORÓW',
+  children: [{
+    label: 'Ogród sztuk (zajęcia interdyscyplinarne)',
+    event: [{
+      day: 'poniedziałek',
+      hour: '10:00-12:00',
+      time: '2,0 godz.'
+    }]
+  }]
 }]
