08a100c98abbc74b16bbebde6b5cb48da5b2e652
Michal Wieja <m.wieja@qarbon.it>
2022-11-06 19:58:54 +0100

    oferta, modal

---
+++ components/ModalComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="modal-overlay">
+    <div class="modal">
+      <div class="close" @click="$emit('close-modal')">
+        &times;
+      </div>
+      <div class="content">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalComponent'
+}
+</script>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #000000da;
+  z-index: 10;
+}
+
+.modal {
+  text-align: center;
+  background-color: white;
+  min-height: 200px;
+  min-width: 300px;
+  max-width: 600px;
+  margin-top: 10%;
+  padding: 60px 40px;
+  border-radius: 20px;
+  position: relative;
+}
+
+.close {
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  font-size: 32px;
+  font-family: sans-serif;
+  cursor: pointer;
+}
+
+</style>
--- sections/home/OfferSection.vue
+++ sections/home/OfferSection.vue
@@ -1,39 +1,85 @@
 <template>
   <div id="offer" class="offer site container">
-    <SectionTitle subtitle="Co u nas znajdziesz" title="Oferta" />
+    <SectionTitle
+      subtitle="Zajęcia dla maluszków i przedszkolaków"
+      title="Oferta"
+    />
     <div class="offer__cards">
       <div
-        v-for="card in offerConfig"
+        v-for="card in youngestOfferConfig"
         :key="card.label"
         class="offer__card"
+        @click="()=>handleClick(card)"
+      >
+        <img :src="`offer/${card.photo}`" alt="" class="top-img">
+        <img :src="`offer/${card.photo}`" alt="" class="bg-img">
+        <span class="label">{{ card.label }}</span>
+      </div>
+    </div>
+    <SectionTitle
+      subtitle="Zajęcia dla młodszych"
+    />
+    <div class="offer__cards">
+      <div
+        v-for="card in youngerOfferConfig"
+        :key="card.label"
+        class="offer__card"
+        @click="()=>handleClick(card)"
+      >
+        <img :src="`offer/${card.photo}`" alt="" class="top-img">
+        <img :src="`offer/${card.photo}`" alt="" class="bg-img">
+        <span class="label">{{ card.label }}</span>
+      </div>
+    </div>
+    <SectionTitle
+      subtitle="Zajęcia dla starszych"
+    />
+    <div class="offer__cards">
+      <div
+        v-for="card in olderOfferConfig"
+        :key="card.label"
+        class="offer__card"
+        @click="()=>handleClick(card)"
       >
         <img alt="" class="top-img" src="offer/rysunek.jpg">
         <img alt="" class="bg-img" src="offer/rysunek.jpg">
         <span class="label">{{ card.label }}</span>
       </div>
     </div>
+
+    <ModalComponent v-if="activeCard" @close-modal="activeCard=null">
+      <div>{{ activeCard.desc }}</div>
+    </ModalComponent>
   </div>
 </template>
 
 <script>
 import SectionTitle from '../../components/SectionTitle.vue'
-import { offerConfig } from '../../config/offer-config.js'
+import {
+  olderOfferConfig,
+  youngerOfferConfig,
+  youngestOfferConfig
+} from '../../config/offer-config.js'
+import ModalComponent from '../../components/ModalComponent.vue'
 
 export default {
   name: 'OfferSection',
-  components: { SectionTitle },
+  components: {
+    ModalComponent,
+    SectionTitle
+  },
   data () {
     return {
-      offerConfig
+      youngestOfferConfig,
+      youngerOfferConfig,
+      olderOfferConfig,
+      activeCard: null
+    }
+  },
+  methods: {
+    handleClick (el) {
+      this.activeCard = el
     }
   }
-
 }
 </script>
-
-<style scoped>
-.offer {
-  min-height: 100vh;
-}
-
-</style>
