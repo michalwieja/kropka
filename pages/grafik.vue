0f8f1249613d7b1b736942ca1842fe3c309b6968
Michal Wieja <m.wieja@qarbon.it>
2023-03-17 12:21:12 +0100

    added day of the week in grafik

--- pages/grafik.vue
+++ pages/grafik.vue
@@ -12,7 +12,7 @@
     </div>
     <div v-for="(date, key) in groupByDate(filteredItems)" :key="date.key">
       <div class="schedule__group-label">
-        {{ $dayjs(key).format('DD MMMM, dddd') }}
+        {{ key }}, {{ date[0].day_of_week }}
       </div>
       <div v-for="event in date" :key="event.id" class="schedule__event-row">
         <div style="flex: 1">
