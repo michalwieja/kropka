1d71069a7428a4ad6111b80648d47fdf668d13d0
mwieja <m.wieja@qarbon.it>
2023-04-24 15:13:24 +0200

    calendar update

--- pages/grafik.vue
+++ pages/grafik.vue
@@ -53,20 +53,14 @@ export default {
     }
   },
   computed: {
-    currentItems () {
-      return this.calendar.filter(el => this.$dayjs(el.date, 'DD MMMM YYYY').locale('pl').isAfter(this.$dayjs().subtract(1, 'day')))
-    },
     filteredItems () {
       if (this.filter) {
-        return this.currentItems.filter(el => el.title === this.filter)
+        return this.calendar.filter(el => el.title === this.filter)
       }
-      return this.currentItems
+      return this.calendar
     }
   },
   methods: {
-    handleFilter (filter) {
-      this.filter = filter
-    },
     groupByDate (events) {
       return events.reduce((result, event) => {
         const date = event.date
