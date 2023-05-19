4145ebad57cccc44cd4f178bf1ef810479694176
michalwieja <mwieja@yahoo.com>
2023-05-19 11:37:24 +0200

    api key deleted

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -2,5 +2,5 @@ import Vue from 'vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
-  load: { key: 'AIzaSyBMEuWmFwDROrO91Xbv6OW3hB1wG92Oqnw' }
+  load: { key: '' }
 })
