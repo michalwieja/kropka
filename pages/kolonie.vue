4ddba91753b3ea6f002f7154aed1f5bedf74a92d
michalwieja <mwieja@yahoo.com>
2023-05-26 14:57:26 +0200

    polkolonie plakat

--- pages/kolonie.vue
+++ pages/kolonie.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="site container workshops">
     <SectionTitle subtitle="Letnie Warsztaty Twórcze dla Dzieci" title="Półkolonie" />
-    <ContentWithImage filename="./offer/kolonie-1.jpg">
+    <ContentWithImage filename="./kolonie.jpg">
       <p class="summary">
         Zapraszamy dzieci w wieku od 6 do 12 lat na pełne kreatywnych przygód letnie warsztaty, które
         odbędą się w terminach od 26 czerwca do 1 września 2023 roku. Nasze warsztaty plastyczne i ruchowe
@@ -9,8 +9,6 @@
         dzieci. Przez cały dzień zapewniamy opiekę, posiłki oraz wiele inspirujących zajęć w naszej przestronnej
         pracowni i ogrodzie.
       </p>
-    </ContentWithImage>
-    <ContentWithImage filename="./offer/kolonie-2.jpg" reverse>
       <h2>Informacje o Warsztatach</h2>
       <ul>
         <li>Opieka w godzinach od 7:00-17:00</li>
@@ -19,37 +17,38 @@
         <li>Dzieci mają do dyspozycji 150m<sup>2</sup> pracowni plus ogród</li>
         <li>W cenie: opieka, warsztaty, wyżywienie (drugie śniadanie, obiad, podwieczorek, przekąski, woda/herbata bez ograniczeń)</li>
       </ul>
+
+      <div class="row">
+        <div class="column">
+          <h2>Warsztaty Plastyczne</h2>
+          <ul>
+            <li>Plener malarski i rysunkowy</li>
+            <li>Landart czyli sztuka ziemi</li>
+            <li>Cyjanotypia czyli jedna z najstarszych technik fotograficznych</li>
+            <li>Ebru czyli malowanie na wodzie</li>
+            <li>Linoryt</li>
+            <li>Mydlarnia czyli jak się robi mydełka</li>
+          </ul>
+        </div>
+        <div class="column">
+          <h2>Warsztaty Ruchowe</h2>
+          <ul>
+            <li>Taneczne (zumba)</li>
+            <li>Podchody leśne</li>
+            <li>Bushcraft</li>
+            <li>Przyrodnicze</li>
+            <li>Aquapark</li>
+          </ul>
+        </div>
+      </div>
+      <h2>Plan Dnia</h2>
+      <p class="day-plan">
+        <strong>7:00-8:30:</strong> Przyprowadzanie dzieci - planszówki, praktyka uważności, muzykowanie, zabawy grupowe<br>
+        <strong>8:30-12:00:</strong> Warsztat I + drugie śniadanie<br>
+        <strong>12:00-15:30:</strong> Warsztat II + przerwa obiadowa<br>
+        <strong>15:30-17:00:</strong> Odbieranie dzieci - planszówki, praktyka uważności, muzykowanie, zabawy grupowe
+      </p>
     </ContentWithImage>
-    <div class="row">
-      <div class="column">
-        <h2>Warsztaty Plastyczne</h2>
-        <ul>
-          <li>Plener malarski i rysunkowy</li>
-          <li>Landart czyli sztuka ziemi</li>
-          <li>Cyjanotypia czyli jedna z najstarszych technik fotograficznych</li>
-          <li>Ebru czyli malowanie na wodzie</li>
-          <li>Linoryt</li>
-          <li>Mydlarnia czyli jak się robi mydełka</li>
-        </ul>
-      </div>
-      <div class="column">
-        <h2>Warsztaty Ruchowe</h2>
-        <ul>
-          <li>Taneczne (zumba)</li>
-          <li>Podchody leśne</li>
-          <li>Bushcraft</li>
-          <li>Przyrodnicze</li>
-          <li>Aquapark</li>
-        </ul>
-      </div>
-    </div>
-    <h2>Plan Dnia</h2>
-    <p class="day-plan">
-      <strong>7:00-8:30:</strong> Przyprowadzanie dzieci - planszówki, praktyka uważności, muzykowanie, zabawy grupowe<br>
-      <strong>8:30-12:00:</strong> Warsztat I + drugie śniadanie<br>
-      <strong>12:00-15:30:</strong> Warsztat II + przerwa obiadowa<br>
-      <strong>15:30-17:00:</strong> Odbieranie dzieci - planszówki, praktyka uważności, muzykowanie, zabawy grupowe
-    </p>
   </div>
 </template>
 
