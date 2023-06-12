4443dcd80cd59628cfdc5fa4d79b1bff4dbfe354
michalwieja <mwieja@yahoo.com>
2023-06-12 22:47:30 +0200

    birthday update

--- pages/urodziny.vue
+++ pages/urodziny.vue
@@ -1,9 +1,36 @@
 <template>
   <div class="site container birthday">
-    <SectionTitle subtitle="Artystycznie, Kreatywnie, Zdrowo" title="Uroczyste Urodziny" />
+    <SectionTitle
+      subtitle="Świętowanie + warsztaty kreatywne"
+      title="Artystyczne urodziny w Kropce"
+    />
+    <h2>
+      Urodzinki dla dzieci od 4 roku życia. Sprawdź nasze propozycje. Wybierz warsztaty, napisz ilu gości zapraszasz, skomponuj dodatki i świętuj swoje urodziny!
+    </h2>
     <div class="content">
       <ContentWithImage filename="/gallery/8D1A0107.jpg">
-        <h3>Wybierz warsztaty:</h3>
+        <h3>
+          Ile to kosztuje:
+        </h3>
+        <h3>Cena i dodatkowe informacje:</h3>
+        <p>
+          Cena: 550 zł dla 6 osób wraz z solenizantem.<br>
+          Cena: 690 zł dla 10 osób wraz z solenizantem, powyżej 10 osób + 55 zł/kolejny uczestnik.<br>
+          Dla dzieci od 4 roku życia.<br>
+        </p>
+        <h3>W cenie:</h3>
+        <ul>
+          <li>Uczestnictwo w warsztatach</li>
+          <li>Opieka prowadzącego przez cały czas trwania warsztatów</li>
+          <li>Udostępnienie sali po warsztatach</li>
+          <li>Przekąski: owoce, chrupki kukurydziane, wafelki ryżowe, chipsy jabłkowe, paluszki, bakalie, drobne ciasteczka</li>
+          <li>Gorąca czekolada z bitą śmietaną i piankami marshmallow lub deser lodowy dla każdego uczestnika</li>
+          <li>Napoje bez limitu w dzbankach: woda, lemoniada</li>
+          <li>Rezerwacja stolika w kawiarni dla dorosłych gości + woda bez limitu</li>
+        </ul>
+      </ContentWithImage>
+      <ContentWithImage reverse filename="/gallery/8D1A9909.jpg">
+        <h3>Wybierz swoje urodzinowe warsztaty:</h3>
         <ul>
           <li>Pracownia ceramiki - zabawa w glinie*</li>
           <li>Pracownia malarska - malowanie sztalugowe na płótnie 50x70 cm z wykorzystaniem techniki akrylowej lub malowanie akwarelowe na specjalnym papierze</li>
@@ -20,26 +47,10 @@
         ** W przypadku warsztatów "Maly architekt" powstaje jedna makieta dla solenizanta, pozostali uczestnicy zabierają wykonaną przez siebie pracę do domu.<br>
         *** Wybór techniki zależy od wieku uczestników.
       </ContentWithImage>
-      <ContentWithImage reverse filename="/gallery/8D1A9909.jpg">
-        <h3>Cena i dodatkowe informacje:</h3>
-        <p>
-          Cena: 550 zł dla 6 osób wraz z solenizantem.<br>
-          Cena: 690 zł dla 10 osób wraz z solenizantem, powyżej 10 osób + 55 zł/kolejny uczestnik.<br>
-          Dla dzieci od 4 roku życia.<br>
-        </p>
-
-        <h3>W cenie:</h3>
-        <ul>
-          <li>Uczestnictwo w warsztatach</li>
-          <li>Opieka prowadzącego przez cały czas trwania warsztatów</li>
-          <li>Udostępnienie sali po warsztatach</li>
-          <li>Przekąski: owoce, chrupki kukurydziane, wafelki ryżowe, chipsy jabłkowe, paluszki, bakalie, drobne ciasteczka</li>
-          <li>Gorąca czekolada z bitą śmietaną i piankami marshmallow lub deser lodowy dla każdego uczestnika</li>
-          <li>Napoje bez limitu w dzbankach: woda, lemoniada</li>
-          <li>Rezerwacja stolika w kawiarni dla dorosłych gości + woda bez limitu</li>
-        </ul>
-
-        <h3>Dodatkowe opcje:</h3>
+      <div class="info">
+        <h3>
+          Jeśli chcesz możesz wybrać dodatkowe opcje  takie jak:
+        </h3>
         <ul>
           <li>Ciasta/torty - możliwość zamówienia ciasta w kawałkach</li>
           <li>Wniesienie własnego tortu/ciasta za opłatą 40 zł</li>
@@ -50,7 +61,7 @@
           <li>Tosty z serem i szynką</li>
           <li>20% zniżka na kawę/herbatę dla dorosłych gości</li>
         </ul>
-      </ContentWithImage>
+      </div>
     </div>
   </div>
 </template>
